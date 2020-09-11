e287e708098256c1a1dd5e0558c831a8c2f03f57
asianspirit <arslanshirazhiyev@gmail.com>
2020-09-12 00:44:45 +0600

    added score and carrect functions

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 
 const score = document.querySelector('.score'),
-       start = document.querySelector('.start'),
-       gameArea = document.querySelector('.game-area'),
-       car = document.createElement('div');
+    start = document.querySelector('.start'),
+    gameArea = document.querySelector('.game-area'),
+    car = document.createElement('div');
 
 
 car.classList.add('car');
@@ -30,50 +30,59 @@ const setting = {
 }
 
 function getQuantityElements(heightElement) {
-   return  document.documentElement.clientHeight / heightElement + 1;
+    return document.documentElement.clientHeight / heightElement + 1;
 }
 
-function startGame () {
+function startGame() {
     start.classList.add('hide');
-for(let i = 0; i < getQuantityElements(100); i++) {
-     const line = document.createElement('div');
-     line.classList.add('line');
-     line.style.top = (i * 100) + 'px';
-     line.y = i * 100;
-     gameArea.appendChild(line);
-}
+    gameArea.innerHTML = '';
 
- for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
-     const enemy = document.createElement('div'); 
-     enemy.classList.add('enemy');
-     enemy.y = -100 * setting.traffic * (i + 1);
-     enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
-     enemy.style.top = enemy.y + 'px';
-     enemy.style.background = 'transparent  url(./image/enemy.png) center / cover no-repeat;';
-     gameArea.appendChild(enemy);
- }
+    for (let i = 0; i < getQuantityElements(100); i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
 
+    for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y = -100 * setting.traffic * (i + 1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent  url(./image/enemy.png) center / cover no-repeat;';
+        gameArea.appendChild(enemy);
+    }
+    setting.score = 0; //Обнуление очков в начале игры
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
- function playGame() {  
-     if (setting.start) {
+function playGame() {
+    if (setting.start) {
+        //Счет очков
+        setting.score += setting.speed;
+        //Вывод очков
+        score.innerHTML = 'Score:<br>' + setting.score;
         moveRoad();
         moveEnemy();
-         if(keys.ArrowLeft && setting.x > 0) {
-             setting.x -= setting.speed;
-         }
-         if(keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+        if (keys.ArrowLeft && setting.x > 0) {
+            setting.x -= setting.speed;
+        }
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
             setting.x += setting.speed;
         }
-        if(keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
             setting.y += setting.speed;
         }
-        if(keys.ArrowUp && setting.y > 0) {
+        if (keys.ArrowUp && setting.y > 0) {
             setting.y -= setting.speed;
         }
 
@@ -81,15 +90,15 @@ for(let i = 0; i < getQuantityElements(100); i++) {
         car.style.top = setting.y + 'px';
 
         requestAnimationFrame(playGame);
-     }  
- }
-
-function startRun (event) {
-   event.preventDefault();
-   keys[event.key] = true;
+    }
 }
 
-function stopRun (event) {
+function startRun(event) {
+    event.preventDefault();
+    keys[event.key] = true;
+}
+
+function stopRun(event) {
     event.preventDefault();
     keys[event.key] = false;
 }
@@ -97,25 +106,40 @@ function stopRun (event) {
 
 function moveRoad() {
     let lines = document.querySelectorAll('.line');
-    lines.forEach(function(line) {
-          line.y += setting.speed;
-          line.style.top = line.y + 'px';
+    lines.forEach(function (line) {
+        line.y += setting.speed;
+        line.style.top = line.y + 'px';
 
-          if(line.y >= document.documentElement.clientHeight) {
-              line.y = -100;
-          }
+        if (line.y >= document.documentElement.clientHeight) {
+            line.y = -100;
+        }
     });
 }
 
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
-    enemy.forEach(function(item){
-      item.y += setting.speed / 2;
-      item.style.top = item.y + 'px';
-      if (item.y >= document.documentElement.clientHeight){
-        item.y = -100 * setting.traffic;
-        item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
-    }
+
+
+    enemy.forEach(function (item) {
+        //carRect - получает параметры автомобиля
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+        //Проверка на столкновение
+        if (carRect.top <= enemyRect.bottom && carRect.right >= enemyRect.left && carRect.left <= enemyRect.right && carRect.bottom >= enemyRect.top) {
+            setting.start = false;
+            console.warn('ДТП');
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight;
+        }
+
+        item.y += setting.speed / 2;
+        item.style.top = item.y + 'px';
+        if (item.y >= document.documentElement.clientHeight) {
+            item.y = -100 * setting.traffic;
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        }
     });
 
 }
+
+// метод getBoundingClientRect Возвращает размеры и позиции элемента в виде объекта
